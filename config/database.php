c51629a2c89674b94602d87165e7cce2455cc2c0
Grace Siagian <gracessiagian@gmail.com>
2026-09-10 15:44:32 +0700

    Update database.php
    Fix PDO constant error

--- config/database.php
+++ config/database.php
@@ -22,8 +22,7 @@ function get_db(): PDO {
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . DB_CHARSET . " COLLATE utf8mb4_unicode_ci"
+            PDO::ATTR_EMULATE_PREPARES   => false
         ];
 
         try {
